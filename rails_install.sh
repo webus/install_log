e4107f42218b9d7d577b30618c5b1c63e99c82e6
webus <webusnix@gmail.com>
2013-04-05 15:06:12 +0400

    fix

---
+++ rails_install.sh
@@ -0,0 +1,17 @@
+curl -L https://get.rvm.io | bash -s stable
+sudo apt-get install binutils
+sudo apt-get install gcc
+sudo apt-get install git-core
+sudo apt-get install autoconf
+sudo apt-get install bison
+sudo apt-get install sqlite3
+sudo apt-get install libreadline6-dev
+sudo apt-get install zlib1g-dev
+sudo apt-get install libssl-dev libyaml-dev libsqlite3-dev libxml2-dev libxslt1-dev libgdbm-dev libncurses5-dev libtool libffi-dev
+rvm install 1.9.2
+rvm use 1.9.2
+/bin/bash --login
+rvm use 1.9.2
+rvm use 1.9.2 --default
+gem install rails
+sudo apt-get install nodejs
