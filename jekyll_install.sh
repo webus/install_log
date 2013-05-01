dbd87ecf39d0aad9b690797db66d0e9e9ff6265c
webus <webusnix@gmail.com>
2013-05-01 23:00:16 +0400

    python added

--- jekyll_install.sh
+++ jekyll_install.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-gem install jekyll
+#gem install jekyll
+gem install rdiscount --ruby=/home/webus/.rvm/rubies/ruby-1.9.2-p320/bin/ruby
+#jekyll --rdiscount
---
+++ python_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sudo apt-get install python
+sudo apt-get install python-pip python-dev build-essential
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-wsgi
