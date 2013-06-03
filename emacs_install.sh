c7dbc89c89a692c249c00ffc68f42a0b737ef7d1
webus <webusnix@gmail.com>
2013-06-03 21:44:42 +0400

    added install script for emacs. oldest commits too.

---
+++ emacs_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#https://launchpad.net/~cassou/+archive/emacs
+sudo add-apt-repository ppa:cassou/emacs
+sudo apt-get update
+sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg
