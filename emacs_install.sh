4988f38eb2bde64ef4a0f832941de8da42a8ebd2
webus <webusnix@gmail.com>
2013-11-15 20:34:13 +0400

    update emacs

--- emacs_install.sh
+++ emacs_install.sh
@@ -3,3 +3,8 @@
 sudo add-apt-repository ppa:cassou/emacs
 sudo apt-get update
 sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg
+
+#if install from source
+sudo apt-get install build-essential texinfo libx11-dev libxpm-dev libjpeg-dev libpng-dev libgif-dev libtiff-dev libgtk2.0-dev libncurses-dev 
+
+#http://askubuntu.com/questions/213873/what-library-i-need-to-install-if-i-want-to-compile-emacs
